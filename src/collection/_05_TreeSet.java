f154297524b546f9ea536d0ed6d0d0b938e3dedc
sjiyeong12 <sjiyeong72@gmail.com>
2022-06-28 16:24:45 +0900

    commit

--- src/collection/_05_TreeSet.java
+++ src/collection/_05_TreeSet.java
@@ -11,10 +11,26 @@ public class _05_TreeSet {
         lotto.add(ball);
         }
         System.out.println(lotto);
+
+
+        Set set = new TreeSet();
+        Ball[] balls = new Ball[5];
+        int[] nums = {4,2,1,5,3};
+        String[] colors = {"red", "yellow", "black", "pink", "green"};
+        Ball ball1 = new Ball(5,"Red");
+        Ball ball2 = new Ball(5,"Red");
+        Ball ball3 = new Ball(5,"Red");
+        Ball ball4 = new Ball(5,"Red");
+        Ball ball5 = new Ball(5,"Red");
     }
 }
 
 class Ball{
     int num;
     String color;
+    public Ball(int num, String color){
+        this.num=num;
+        this.color=color;
+    }
+
 }
