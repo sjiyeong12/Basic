894b1b892c0c4b30067aef95ea8d0efc3ca5d1e1
sjiyeong12 <sjiyeong72@gmail.com>
2022-07-01 17:53:29 +0900

    commit

--- src/io/_02_NotePad.java
+++ src/io/_02_NotePad.java
@@ -1,7 +1,45 @@
 package io;
 
-public class _02_NotePad extends Base Frm{
-    public static void main(String[] args) {
-        new _02_NotePad();
-    }
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollBar;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+import swing.BaseFrm;
+
+public class _02_Notepad extends BaseFrm {
+public static void main(String[] args) {
+new _02_Notepad();
+}
+private JMenuBar menuBar;
+private JMenu mnFile, mnInfo;
+private JMenuItem miOpen, miNew, miSave, miExit;
+private JTextArea ta;
+private JScrollPane scp;
+@Override
+public void init() {
+menuBar = new JMenuBar();
+mnFile = new JMenu("File(f)");
+mnInfo = new JMenu("Info(I)");
+miNew = new JMenuItem("New(N)");
+miOpen = new JMenuItem("Open(O)");
+miSave = new JMenuItem("Save(S)");
+miExit = new JMenuItem("Exit(x)");
+mnFile.add(miNew);
+mnFile.add(miOpen);
+mnFile.add(miSave);
+mnFile.add(miExit);
+menuBar.add(mnFile);
+menuBar.add(mnInfo);
+setJMenuBar(menuBar);
+ta = new JTextArea();
+scp = new JScrollBar(tq);
+}
+
+@Override
+public void arrange() {
+    add(scp)l
+}
 }
